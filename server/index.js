29684ef7bd7ef26eecfe7e7b25c6f05032ee95f9
jeffreyadamo <jeffreyadamo@gmail.com>
2021-06-07 17:39:15 -0700

    express server setup

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'url shortener'
+  });
+});
+
+// app.get('/:id', (req, res) => {
+//   // TODO: redirect to url 
+// });
+// app.post('/url', (req, res) => {
+//   // TODO: create a short url 
+// });
+// app.get('/url/:id', (req, res) => {
+//   // TODO: get a short url by id 
+// });
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {  
+  console.log(`Listening a http://localhost:${port}`);
+});
